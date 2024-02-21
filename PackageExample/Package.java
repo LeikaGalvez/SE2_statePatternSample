ab4056ba55be9cb80684874364a1458567d07844
LeikaGalvez <142652629+LeikaGalvez@users.noreply.github.com>
2024-02-21 15:17:21 +0000

    Organized and added a sampleDemo for statePattern.

---
+++ PackageExample/Package.java
@@ -0,0 +1,27 @@
+package PackageExample;
+
+public class Package {
+    private PackageState state = new OrderedState();
+
+    // getter, setter
+
+    public void previousState() {
+        state.prev(this);
+    }
+
+    public void nextState() {
+        state.next(this);
+    }
+
+    public void printStatus() {
+        state.printStatus();
+    }
+
+    public void setState(PackageState state) {
+        this.state=state;
+    }
+
+    public PackageState getState(PackageState state) {
+        return state;
+    }
+}
---
+++ PackageExample/PackageState.java
@@ -0,0 +1,7 @@
+package PackageExample;
+
+public interface PackageState {
+    void next(Package pkg);
+    void prev(Package pkg);
+    void printStatus();
+}
