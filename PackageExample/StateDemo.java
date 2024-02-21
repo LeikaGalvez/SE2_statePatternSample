687c57670fbf5765a740980214034893e74e5e22
LeikaGalvez <142652629+LeikaGalvez@users.noreply.github.com>
2024-02-21 15:21:14 +0000

    Added notes to main.

--- PackageExample/StateDemo.java
+++ PackageExample/StateDemo.java
@@ -4,15 +4,19 @@ public class StateDemo {
     public static void main(String[] args) {
 
         Package pkg = new Package();
+        //package ordered ; default state
         pkg.printStatus();
 
         pkg.nextState();
+        //orderedState->deliveredState ; package delivered
         pkg.printStatus();
 
         pkg.nextState();
+        //deliveredState->ReceivedState ; package received
         pkg.printStatus();
 
         pkg.nextState();
+        //receivedState->nothing ; print out "package received"
         pkg.printStatus();
     }
 }
